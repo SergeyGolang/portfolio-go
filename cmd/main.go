6e1846932a8eebf8665fc30683d4b3578d8a1757
SergeyCicada <111425762+SergeyCicada@users.noreply.github.com>
2025-07-04 21:29:09 +0400

    portfolio_go

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/go-chi/chi"
+
+func main() {
+	//TODO: init logger
+
+	//TODO: middleware
+
+	//TODO: init router
+	router := chi.NewRouter()
+
+	//TODO: start server
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SergeyGolang/portfolio-go
+
+go 1.24.4
+
+require github.com/go-chi/chi v1.5.5
